4cb02672ca2ce6e0829a3222f58a2d7952e246e5
imvickykumar999 <imvickykumar999@gmail.com>
2022-06-14 17:03:42 +0530

    ir remote

---
+++ IR_Remote/IR_Remote.ino
@@ -0,0 +1,22 @@
+
+//https://www.tinkercad.com/things/jifiCImBU2C
+
+#include <IRremote.h>
+int RECV_PIN = 11;
+
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    irrecv.resume(); // Receive the next value    
+  }
+  delay(100);
+}
